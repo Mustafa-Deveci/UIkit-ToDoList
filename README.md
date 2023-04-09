ae26fa1a1717d7d4515fbc4433b95ca7d20862c7
Mustafa-Deveci <m.deveci9@hotmail.com>
2023-04-09 16:36:43 +0300

    updated delete tasks.

---
+++ README.md
@@ -0,0 +1,6 @@
+# UIkit-To-Do-List
+This project is a simple to-do list application written in the swift programming language. Tableview and alert were used.
+##
+<img width="250" alt="photo1" src="https://user-images.githubusercontent.com/79964936/209445381-e112e75c-73c1-421e-a10e-af7dea48be0b.png"><img width="250" alt="photo2" src="https://user-images.githubusercontent.com/79964936/209445400-369a6f7d-39e2-4dbd-b246-f8abf7284080.png">
+##
+<img width="250" alt="photo3" src="https://user-images.githubusercontent.com/79964936/209445403-3698b34d-3bec-4699-918f-09362336fec0.png"><img width="250" alt="photo4" src="https://user-images.githubusercontent.com/79964936/209445483-1bca11a9-62e7-47c3-9924-f8f81b5b0078.png">
---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,96 @@
+//  ViewController.swift
+//  Created by mustafa deveci on 9.10.2022.
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var tasks = [String]()
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        tableView.allowsMultipleSelectionDuringEditing = true
+        self.tasks = UserDefaults.standard.stringArray(forKey: "items") ?? []
+        title = "To Do List"
+        tableView.dataSource = self
+        tableView.delegate = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+        
+    }
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "New Task", message: "Enter a new to do list item!" , preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = "Enter a task ... "
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self](_) in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    
+                    DispatchQueue.main.async {
+                        var currentItems = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentItems.append(text)
+                        UserDefaults.standard.setValue(currentItems, forKey: "items")
+                        
+                        self?.tasks.append(text)
+                        self?.tableView.reloadData()
+                        
+                        if let selectedRowIndexPath = self?.tableView.indexPathForSelectedRow {
+                            self?.tableView.deselectRow(at: selectedRowIndexPath, animated: true)
+                        }
+                    }
+                }
+            }
+        }))
+        
+        present(alert,animated: true )
+    }
+}
+
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        let selectedCell = tableView.cellForRow(at: indexPath)
+        selectedCell?.isSelected = false
+        let alert = UIAlertController(title: "Delete task", message: "Are you sure you want to delete this task?", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            self.tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { (action) in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                tableView.performBatchUpdates({
+                    if let tasks = UserDefaults.standard.stringArray(forKey: "tasks") {
+                        var currentItems = tasks
+                        currentItems.remove(at: indexPath.row)
+                        UserDefaults.standard.setValue(currentItems, forKey: "tasks")
+                    }
+                    self.tasks.remove(at: indexPath.row)
+                    self.tableView.deleteRows(at: [indexPath], with: .fade)
+                    
+                    
+                }, completion: nil)
+            }
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(deleteAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+}
